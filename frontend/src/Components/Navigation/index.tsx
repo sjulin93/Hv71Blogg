c73c3d4e312a1d74192893ee2a36c54616a0e611
sjulin93 <sjulin.simon@gmail.com>
2024-03-24 16:48:18 +0100

    HV-1 nav och footer

---
+++ frontend/src/Components/Navigation/index.tsx
@@ -0,0 +1,93 @@
+import LogoImage from '../../assets/Images/hv71logga.png'
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Text,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon, AddIcon } from "@chakra-ui/icons";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Links = ["Inlägg", "Om oss", "Kontakta oss"];
+
+const NavLink = (props: Props) => {
+  const { children } = props;
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.300", "gray.700"),
+      }}
+      href={"#"}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default function Navigation() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box bg="blue.hvBlue" px={4} color="white">
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>
+              <img src={LogoImage} alt="hv71" />
+            </Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <Button variant={"solid"} size={"sm"} mr={4}>
+              Logga in
+            </Button>
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import * as React from "react";
+import { ChakraProvider , extendTheme} from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const colors = {
+  blue : {
+    800: '#2A4365',
+    900: '#1A365D',
+    hvBlue: '#0a2240'
+  },
+  white : {
+    500: 'RGBA(255, 255, 255, 0.36)',
+    600: 'RGBA(255, 255, 255, 0.48)'
+  }
+}
+
+const body = {
+  height: '100dvh'
+}
+const theme = extendTheme({colors, body});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme = {theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
